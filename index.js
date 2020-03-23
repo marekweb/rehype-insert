67c112237c0d1dd57d05dfadbbb8459ef9f49185
Marek <marekz@gmail.com>
2020-03-23 14:06:54 -0400

    Move utils out of the repository

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const hastUtilInsert = require("./hast-util-insert");
+const hastUtilInsert = require("hast-util-insert");
 
 function rehypeInsert(options = {}) {
-  return function rehypeInsertTransformer(tree, vfile) {
+  return function rehypeInsertTransformer(tree) {
     const { insertions = [] } = options;
     insertions.forEach(function (i) {
       const result = hastUtilInsert(tree, i.selector, i.insert, i.action);
